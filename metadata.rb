5f963793c8b31602ac5f751ad11c6cefef81baa2
Adam Kocoloski <adam@cloudant.com>
2012-03-23 11:48:37 -0400

    Allow newer versions of the apt cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ license           "Apache 2.0"
 description       "Installs/Configures Librato Silverline"
 version           "0.0.8"
 
-depends 'apt', '~> 1.2.0'
+depends 'apt', '>= 1.2.0'
 
 %w{ubuntu debian}.each do |os|
   supports os
